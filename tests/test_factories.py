b97123399f685202405572e5a3b92a71d6741074
Jules Robichaud-Gagnon <j.robichaudg@gmail.com>
2017-06-21 20:55:26 -0400

    Splits tests in different files.

---
+++ tests/test_factories.py
@@ -0,0 +1,51 @@
+from django.contrib.auth.models import User
+from django.test import TestCase
+
+from my_app import models
+from tests import factories
+
+
+class TestUserFactory(TestCase):
+
+    def test_create_user(self):
+        user = factories.UserFactory()
+        self.assertTrue(User.objects.filter(pk=user.pk).exists())
+
+    def test_username_is_unique(self):
+        user1 = factories.UserFactory()
+        user2 = factories.UserFactory()
+        self.assertNotEqual(user1.username, user2.username)
+
+
+class TestEntryFactory(TestCase):
+
+    def test_can_create_blog_entry(self):
+        with self.subTest('Entry values are already filled'):
+            entry = factories.EntryFactory()
+            self.assertTrue(models.Entry.objects.filter(pk=entry.pk).exists())
+            self.assertTrue(models.Blog.objects.filter(pk=entry.blog.pk).exists())
+
+        with self.subTest('Entry generated is already full_clean'):
+            entry.full_clean()
+
+    def test_factory_can_create_many_to_many(self):
+        with self.subTest('Can add authors to the factory'):
+            author = factories.AuthorFactory()
+            entry = factories.EntryFactory(
+                authors=(author,)
+            )
+            self.assertEqual(1, entry.authors.count())
+
+        with self.subTest('Traits can simplify greatly some usage'):
+            entry = factories.EntryFactory(
+                with_authors=True
+            )
+            self.assertTrue(entry.authors.exists())
+
+
+class TestRelatedFactory(TestCase):
+
+    def test_can_create_blog_with_existing_entry(self):
+        blog = factories.BlogWithEntryFactory()
+
+        self.assertTrue(blog.entry_set.exists())
--- tests/test_models.py
+++ tests/test_models.py
@@ -5,7 +5,6 @@ from django.db import IntegrityError, transaction
 from django.test import TestCase
 
 from my_app import models
-from tests import factories
 
 
 class TestUser(TestCase):
@@ -28,19 +27,7 @@ class TestUser(TestCase):
         self.assertTrue(User.objects.filter(username='bar').exists())
 
 
-class TestUserFactory(TestCase):
-
-    def test_create_user(self):
-        user = factories.UserFactory()
-        self.assertTrue(User.objects.filter(pk=user.pk).exists())
-
-    def test_username_is_unique(self):
-        user1 = factories.UserFactory()
-        user2 = factories.UserFactory()
-        self.assertNotEqual(user1.username, user2.username)
-
-
-class TestUserFixtureFactory(TestCase):
+class TestUserFixture(TestCase):
     fixtures = ['test-user']
 
     def test_user_exists(self):
@@ -92,37 +79,3 @@ class TestEntryModel(TestCase):
         author = models.Author.objects.create()
         entry.authors.add(author)
         self.assertTrue(entry.authors.exists())
-
-
-class TestEntryFactory(TestCase):
-
-    def test_can_create_blog_entry(self):
-        with self.subTest('Entry values are already filled'):
-            entry = factories.EntryFactory()
-            self.assertTrue(models.Entry.objects.filter(pk=entry.pk).exists())
-            self.assertTrue(models.Blog.objects.filter(pk=entry.blog.pk).exists())
-
-        with self.subTest('Entry generated is already full_clean'):
-            entry.full_clean()
-
-    def test_factory_can_create_many_to_many(self):
-        with self.subTest('Can add authors to the factory'):
-            author = factories.AuthorFactory()
-            entry = factories.EntryFactory(
-                authors=(author,)
-            )
-            self.assertEqual(1, entry.authors.count())
-
-        with self.subTest('Traits can simplify greatly some usage'):
-            entry = factories.EntryFactory(
-                with_authors=True
-            )
-            self.assertTrue(entry.authors.exists())
-
-
-class TestRelatedFactory(TestCase):
-
-    def test_can_create_blog_with_existing_entry(self):
-        blog = factories.BlogWithEntryFactory()
-
-        self.assertTrue(blog.entry_set.exists())
