d91b717a9f1b4a67dee95c3c4fce4d19bf04af5a
Jules Robichaud-Gagnon <j.robichaudg@gmail.com>
2017-06-21 21:13:08 -0400

    Renames project

--- django_awesome_presentations/wsgi.py
+++ django_awesome_presentations/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for factory_boy_demo project.
+WSGI config for django_awesome_presentations project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "factory_boy_demo.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_awesome_presentations.settings")
 
 application = get_wsgi_application()
--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "factory_boy_demo.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_awesome_presentations.settings")
 
     from django.core.management import execute_from_command_line
 
